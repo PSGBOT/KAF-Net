474abce1a57c5469f67461b6cb58b18ba6ac9d30
Chen Yulin <chenyulin@sjtu.edu.cn>
2025-08-24 14:11:24 +0800

    feat. loader

--- datasets/psr.py
+++ datasets/psr.py
@@ -573,7 +573,7 @@ class PSRDataset(Dataset):
             for cat_idx in range(self.num_func_cat):
                 masks_cat[mask_idx][cat_idx] = 0
 
-        if eval is False:
+        if self.eval is False:
             return {
                 "masked_img": masked_img,
                 "masks_cat": masks_cat,
